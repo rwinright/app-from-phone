480db3bbc2ee10136f7121af0288eece24575195
Roger Winright <rogerwinright@gmail.com>
2019-10-26 01:33:32 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+	<div className="App">
+      <h1 className="header">Heeader!</h1>
+      <p>This is the sub-text</p>
+    </div>
+  );
+}
+
+export default App;
